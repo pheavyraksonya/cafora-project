d3ead04fcd569ea1d09925b8ba36a39912d962e9
Sean Pheavyraksonya <seanphevyraksonya@gmail.com>
2025-07-27 00:52:41 +0700

    update new code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,30 +2,28 @@ import { Facebook, Instagram, Twitter } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-amber-500 to-orange-500 text-white py-6 mt-10">
+    <footer className="bg-orange-500 text-white py-6 mt-10">
       <div className="max-w-5xl mx-auto px-4 flex flex-col items-center gap-3">
-        <p className="text-lg font-semibold">
-          Brewed for Moments That Matter ☕
-        </p>
+        <p className="text-lg font-semibold">Brewed for Moments That Matter</p>
 
         <div className="flex space-x-4">
           <a
             href="https://www.facebook.com/"
             className="hover:text-yellow-100 transition"
           >
-            <Facebook size={20} />
+            <Facebook size={30} />
           </a>
           <a
             href="https://www.instagram.com/?hl=en"
             className="hover:text-yellow-100 transition"
           >
-            <Instagram size={20} />
+            <Instagram size={30} />
           </a>
           <a
             href="https://x.com/home"
             className="hover:text-yellow-100 transition"
           >
-            <Twitter size={20} />
+            <Twitter size={30} />
           </a>
         </div>
 
